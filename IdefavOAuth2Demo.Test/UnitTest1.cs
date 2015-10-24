0b878d1149ca16f3258950e33ee5e43b63e3e5b8
idefav <wzs2009_hi@126.com>
2015-10-24 09:33:00 +0800

    add

---
+++ IdefavOAuth2Demo.Test/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using DotNetOpenAuth.Messaging;
+using DotNetOpenAuth.OAuth2;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IdefavOAuth2Demo.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestService1DoWork()
+        {
+           
+        }
+    }
+}
